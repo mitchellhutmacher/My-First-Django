752db17440c826de66a133a3851986038c7cfdcc
Mitchell Hutmacher <mitchellhutmacher@gmail.com>
2022-07-17 15:24:37 -0500

    Fleshed out the detail view

--- my_first_site/polls/views.py
+++ my_first_site/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
 from .models import Question
@@ -19,10 +19,7 @@ def detail(request, question_id):
     """
     Show the question ID, 404 if there is an invalid question ID
     """
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 
