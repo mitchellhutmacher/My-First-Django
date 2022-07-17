5f67aae92cd2ff176fcf9abf08fb494888c971e6
Mitchell Hutmacher <mitchellhutmacher@gmail.com>
2022-07-17 14:57:27 -0500

    Added the question detail, question results, and question vote stubbed functions and URLs

--- my_first_site/polls/views.py
+++ my_first_site/polls/views.py
@@ -3,5 +3,24 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
     return HttpResponse("Hello, world.  You're at the polls index.  ")
+
+
+def detail(request, question_id):
+    """
+    Show the question ID
+    """
+    resp = "You're looking at question {0}.".format(question_id)
+    return HttpResponse(resp)
+
+
+def results(request, question_id):
+    resp = "You're looking at the results of question {0}.".format(question_id)
+    return HttpResponse(resp)
+
+
+def vote(request, question_id):
+    resp = "You're voting on question {0}.".format(question_id)
+    return HttpResponse(resp)
